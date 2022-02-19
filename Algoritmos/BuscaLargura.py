8a51f21267c70a1664a9239a9fd1ce9672dde180
Eduardo <54189084+EduNP@users.noreply.github.com>
2022-02-19 15:35:59 -0300

    Update BuscaLargura.py

--- Algoritmos/BuscaLargura.py
+++ Algoritmos/BuscaLargura.py
@@ -37,16 +37,16 @@ class AdjuntaNo():
 class Grafo:
     def __init__(self, vertices):
         self.vertices = vertices
-        self.grafo = [None] * self.vertices #Lista de adjacencia do grafo
+        self.lista = [None] * self.vertices #Lista de adjacencia do grafo
 
     def adicionarAresta(self, inicio, destino):
         no = AdjuntaNo(destino) #cria um novo no
-        no.proximo = self.grafo[inicio] #insere na lista de Inicio para destino
-        self.grafo[inicio] = no
+        no.proximo = self.lista[inicio] #insere na lista de Inicio para destino
+        self.lista[inicio] = no
 
         no = AdjuntaNo(inicio)
-        no.proximo = self.grafo[destino] #insere na lsita de Destino para Inicio
-        self.grafo[destino] = no
+        no.proximo = self.lista[destino] #insere na lsita de Destino para Inicio
+        self.lista[destino] = no
 
 def BFS(grafo, vertice):
     # for u in range(grafo.numV):
