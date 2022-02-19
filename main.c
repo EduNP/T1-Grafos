d4c46ae5cb142d614f4c528187552b7f8badb0e9
Eduardo <54189084+EduNP@users.noreply.github.com>
2022-02-19 15:25:21 -0300

    Revert "[Remove] C"
    This reverts commit fcdcb5dc7851b707d6971c90f571d6a64cee034a.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "Algoritmos/BuscaLargura.c"
+#include "Algoritmos/Dijkstra.c"
+#include "Algoritmos/Prim.c"
+#include "Algoritmos/Prufer.c"
+#include <stdio.h>
+
+int main(){
+    //Grafo como matriz de adjacencias, le de arquivos, salva saídas em arquivos
+    //Criar caso de teste
+    teste();
+    //Rodar algoritmos
+    
+    return 0;
+}
